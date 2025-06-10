878006f8471a00b2c0f1bd42316653a0429a837d
Julius <juliuswa98@gmail.com>
2025-06-10 17:25:51 +0200

    remove test stuff

--- src/QuantusJL.jl
+++ src/QuantusJL.jl
@@ -1,14 +1,9 @@
 module QuantusJL
     using PythonCall
-
-    const numpy = Ref{Py}()
-    function __init__()
-        numpy[] = pyimport("numpy")
-    end
-
+    
     const abs_difference(x, y) = begin
-        x_np_array = numpy[].array(x);
-        y_np_array = numpy[].array(y);
+        x_np_array = pyimport("numpy").array(x);
+        y_np_array = pyimport("numpy").array(y);
         similarity_score = pyimport("quantus").functions.similarity_func.abs_difference(x_np_array, y_np_array);
         return pyconvert(Float64, similarity_score)
     end
@@ -16,8 +11,7 @@ module QuantusJL
     export abs_difference;
 end
 
-# using Main.QuantusJL
-
-# a = abs_difference([3.5, 3.0, 3.2], [2.5, 2.0, 2.2])
+using QuantusJL
+a = abs_difference([3.5, 3.0, 3.2], [2.5, 2.0, 2.2])
 
 # typeof(a)
