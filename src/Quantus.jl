56915f68fca562c16bfc74147561bf7ace889614
lwilbert123 <lenawilb@gmail.com>
2025-07-11 21:04:37 +0200

    Quantus module include

--- src/Quantus.jl
+++ src/Quantus.jl
@@ -4,5 +4,15 @@ module Quantus
     export evaluate_faithfulness
 
     include("metrics/python_metrics.jl")
+    include("metrics/RandomLogitJL.jl")
+    include("metrics/MaxSensitivityJL.jl")
+    include("metrics/SparsenessJL.jl")
+
     using .PythonMetrics: evaluate_faithfulness
+    using .RandomLogitJL
+    using .MaxSensitivityJL
+    using .SparsenessJL
+    using .PythonMetrics: evaluate_faithfulness
+
+    export evaluate_faithfulness, RandomLogit, MaxSensitivity, Sparseness
 end
